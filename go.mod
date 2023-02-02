8d4effb46ee74305bdd6df3ecd5838f543785399
Martin Ivanov <raichev93@gmail.com>
2023-02-02 19:12:59 +0200

    json response reade

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/raikata93/ssh-reader
 
 go 1.19
+
+require github.com/araddon/dateparse v0.0.0-20210429162001-6b43995a97de // indirect
--- reader/reader.go
+++ reader/reader.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"encoding/xml"
 	"fmt"
+	"github.com/araddon/dateparse"
 	"io"
 	"net/http"
 	"strings"
@@ -11,14 +12,19 @@ import (
 	"time"
 )
 
+var source, sourceUrl string
+
 type rss struct {
 	Channel channel `xml:"channel"`
 }
 type channel struct {
-	Title string    `xml:"title"`
-	Link  string    `xml:"link"`
+	Title TitleFeed `xml:"title"`
+	Link  LinkFeed  `xml:"link"`
 	Items []RssItem `xml:"item"`
 }
+
+type TitleFeed string
+type LinkFeed string
 
 type RssItem struct {
 	Title       string    `xml:"title" json:"title"`
@@ -29,11 +35,59 @@ type RssItem struct {
 	Description string    `xml:"description" json:"description"`
 }
 
+func (e *TitleFeed) UnmarshalXML(d *xml.Decoder, start xml.StartElement) (err error) {
+	var t xml.Token
+	for t, err = d.Token(); err == nil; t, err = d.Token() {
+		switch tt := t.(type) {
+		case xml.CharData:
+			source = string(tt)
+		}
+	}
+	return nil
+}
+
+func (e *LinkFeed) UnmarshalXML(d *xml.Decoder, start xml.StartElement) (err error) {
+	var t xml.Token
+	for t, err = d.Token(); err == nil; t, err = d.Token() {
+		switch tt := t.(type) {
+		case xml.CharData:
+			sourceUrl = string(tt)
+		}
+	}
+	return nil
+}
+
+func (e *RssItem) UnmarshalXML(d *xml.Decoder, start xml.StartElement) (err error) {
+	tokName := ""
+	var t xml.Token
+	for t, err = d.Token(); err == nil; t, err = d.Token() {
+		switch tt := t.(type) {
+		case xml.StartElement:
+			tokName = tt.Name.Local
+		case xml.CharData:
+			switch tokName {
+			case "title":
+				e.Title = string(tt)
+			case "link":
+				e.Link = string(tt)
+			case "pubDate":
+				e.PublishDate, _ = dateparse.ParseAny(string(tt))
+			case "description":
+				e.Description = string(tt)
+			}
+		case xml.EndElement:
+			tokName = ""
+		}
+	}
+	e.Source = source
+	e.SourceURL = sourceUrl
+	return nil
+}
+
 func Parse(urls string) []byte {
 	var wg sync.WaitGroup
 	var all_result []RssItem
 	results := make(map[string][]RssItem)
-
 	c := make(chan []RssItem)
 	urlSlice := strings.Split(urls, ",")
 	for _, url := range urlSlice {
@@ -65,7 +119,6 @@ func parseXml(url string) []RssItem {
 		fmt.Printf("Error during getXml: %s", err)
 	}
 	rssFeed := rss{}
-
 	err = xml.Unmarshal(xmlBytes, &rssFeed)
 	if err != nil {
 		fmt.Printf("Error during Unmarshal: %s", err)
